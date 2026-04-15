e38bf37b90de6d396ecdab84d3b15018bc7a8461
angie <noga23121993@gmail.com>
2026-04-15 19:23:55 +0200

    p

--- Data/SkillsData/SkillsData.ts
+++ Data/SkillsData/SkillsData.ts
@@ -1,7 +1,7 @@
 export const imgs = [
     { id: 1, src: "/Skills/HTML5.webp", alt: "HTML" },
     { id: 2, src: "/Skills/Css.webp", alt: "CSS" },
-    { id: 3, src: "/Skills/JS.png", alt: "JavaScript" },
+    { id: 3, src: "/Skills/JavaScript.png", alt: "JavaScript" },
     { id: 4, src: "/Skills/Bootstrap.webp", alt: "Bootstrap" },
     { id: 5, src: "/Skills/TailwindCSS.webp", alt: "Tailwind CSS" },
     { id: 6, src: "/Skills/Reactjs.webp", alt: "React" },
